675980f8251ff8c82ad83bb893229fb922919a31
MorningSol20 <moronirsol@gmail.com>
2025-10-29 18:02:40 -0300

    updates: 29/10/2025

--- JS/project.js
+++ JS/project.js
@@ -5,13 +5,12 @@ filterButtons.forEach(button => {
     button.addEventListener('click', () => {
         const filter = button.dataset.filter;
 
-        // remove classe "active" dos outros botões
         filterButtons.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
 
-        // exibe ou esconde os projetos
         projects.forEach(project => {
-            if (filter === 'all' || project.dataset.category === filter) {
+            const categories = project.dataset.category.split(' ');
+            if (filter === 'all' || categories.includes(filter)) {
                 project.classList.remove('hidden');
             } else {
                 project.classList.add('hidden');
